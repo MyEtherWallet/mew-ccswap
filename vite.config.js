bf6d9fa13a83dc0a6db7c8159d3605f841044a76
David Hong <hsurf22@gmail.com>
2022-04-06 02:51:56 -0700

    add https to config

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 const path = require("path");
 
 export default defineConfig({
-    plugins: [
-        vue(),
-    ],
-    resolve: {
-        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+  plugins: [vue()],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
     alias: {
-        "@": path.resolve(__dirname, "./src"),
-        "~@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
+      "~@": path.resolve(__dirname, "./src")
     },
     build: {
-        assetsPublicPath: '',
-        
+      assetsPublicPath: ""
     },
     dev: {
-        assetsPublicPath: '/',
+      assetsPublicPath: "/"
     },
-    vue: 'vue/dist/vue.esm-bundler.js'
-    },
+    vue: "vue/dist/vue.esm-bundler.js"
+  },
+  server: {
+    https: true
+  }
 });
