baefc4eb823b206f2f55443e8a08dccaa152e025
David Hong <hsurf22@gmail.com>
2022-04-13 03:36:06 -0700

    update reCaptcha siteKey

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { VueReCaptcha } from 'vue-recaptcha-v3';
 loadFonts();
 
 createApp(App)
+  // reCaptcha sitekey for testing
   .use(VueReCaptcha, { siteKey: '6LcOkkwfAAAAAHs-bw2WrXHJtcdDWKl7S4JktET9' })
+  //.use(VueReCaptcha, { siteKey: '6LczzE0fAAAAALKiUYa2POyGx41EnRs0zjpAxfOI' })
   .use(vuetify)
   .mount('#app');
