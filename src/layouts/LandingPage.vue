9cb309af4c5dd07ce6a735a9b3960c049f9e7acf
David Hong <hsurf22@gmail.com>
2022-04-15 14:24:59 -0700

    fix landing page mobile layout

--- src/layouts/LandingPage.vue
+++ src/layouts/LandingPage.vue
@@ -6,10 +6,10 @@
   >
     <v-container>
       <v-row>
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="5" lg="6">
           <Header />
         </v-col>
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="7" lg="6">
           <BuyForm />
         </v-col>
       </v-row>
