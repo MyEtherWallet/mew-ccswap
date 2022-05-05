adb47bd5a538b15bc5d9fae297beaea83e5911eb
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-05-05 15:10:16 -0700

    chore: include dist

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
