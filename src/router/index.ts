723fff5bc6ccd9850f73f2dabe1a56acaa07efda
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-05-18 18:00:03 -0700

    chore: router hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
 import LandingPage from "@/layouts/LandingPage.vue";
-
+const history =
+  process.env.NODE_ENV === "production"
+    ? createWebHashHistory("/mew-ccswap/")
+    : createWebHistory();
 const router = createRouter({
-  history: createWebHistory(),
+  history: history,
   routes: [
     {
       path: "/",
