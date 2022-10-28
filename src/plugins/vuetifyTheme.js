4e681620c038317bc7624e0655f1a2b29ce4becb
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2022-10-27 17:43:28 -0700

    devop: theme setup

---
+++ src/plugins/vuetifyTheme.js
@@ -0,0 +1,15 @@
+import colors from "vuetify/lib/util/colors";
+
+export default {
+  theme: {
+    themes: {
+      ethvm: {
+        primary: "#0065FF", // #0065FF
+        secondary: "", // #091E41
+        error: colors.red.accent3,
+        background:
+          "linear-gradient(to left bottom, #0065ff, #0058d9, #004bb4, #003d90, #00306d, #1a2657, #1f1d41, #1c152e, #1d1021, #190a16, #11050b, #000000)",
+      },
+    },
+  },
+};
