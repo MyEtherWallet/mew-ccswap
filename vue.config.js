c8da25a10abec7ca66028adaa48227d269acc253
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-07-16 14:50:10 -0700

    chore: change path

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const webpack = require("webpack");
 const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = defineConfig({
-  publicPath: "./",
   transpileDependencies: true,
   lintOnSave: true,
   configureWebpack: {
