4a198d04d83559ed6d6cc37a9cddaaed43317e32
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-07-31 16:06:34 -0700

    fix: address validation for rs

--- src/components/OrderForm/BuyForm.vue
+++ src/components/OrderForm/BuyForm.vue
@@ -882,20 +882,21 @@ const kdaValidator = (address: string) => {
 };
 
 const addressValid = computed(() => {
+  const address = form.address.toLowerCase();
   return other_chains.includes(form.cryptoSelected)
     ? form.cryptoSelected === "KDA"
-      ? kdaValidator(form.address)
-      : WAValidator.validate(form.address, form.cryptoSelected)
+      ? kdaValidator(address)
+      : WAValidator.validate(address, form.cryptoSelected)
     : !polkadot_chains.includes(form.cryptoSelected)
     ? bitcoin_chains.includes(form.cryptoSelected)
-      ? WAValidator.validate(form.address, form.cryptoSelected)
+      ? WAValidator.validate(address, form.cryptoSelected)
       : props.networkSelected.name === "OP" ||
         props.networkSelected.name === "ARB"
-      ? WAValidator.validate(form.address, "ETH")
-      : WAValidator.validate(form.address, props.networkSelected.name) &&
-        validAddress(form.address)
+      ? WAValidator.validate(address, "ETH")
+      : WAValidator.validate(address, props.networkSelected.name) &&
+        validAddress(address)
     : isValidAddressPolkadotAddress(
-        form.address,
+        address,
         form.cryptoSelected === "DOT" ? 0 : 2
       );
 });
